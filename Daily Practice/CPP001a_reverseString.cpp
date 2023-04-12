c55c4def10583f3bb5fdb59b4094f25623c8bdff
SARIF <sarif.malik@hotmail.com>
2023-04-12 20:37:35 +0530

    day1

--- Daily Practice/CPP001a_reverseString.cpp
+++ Daily Practice/CPP001a_reverseString.cpp
@@ -1,1 +1,42 @@
-fdsa
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str);
+
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
+
+// Q You are given a string s. You need to reverse the string.
+
+
+string reverseWord(string str){
+    
+  //## First Approach 
+  
+//   reverse(str.begin(), str.end());
+    
+    int st = 0, end = str.length()-1; 
+    while(st < end){
+        swap(str[st++], str[end--]); 
+    }
+  
+  return str; 
+}
